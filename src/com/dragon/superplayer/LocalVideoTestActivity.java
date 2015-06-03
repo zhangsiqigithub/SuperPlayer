20c8d4a988fdfdf43b7c0aec7025148c4ebd5170
yeguolong <yeguolong@letv.com>
2015-06-03 19:18:22 +0800

    添加Android默认播放器的Activity

--- src/com/dragon/superplayer/LocalVideoTestActivity.java
+++ src/com/dragon/superplayer/LocalVideoTestActivity.java
@@ -5,22 +5,64 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import com.dragon.superplayer.manager.VideoPlayerManager;
+import com.dragon.superplayer.util.LogUtil;
 
-public class VideoTestActivity extends Activity {
+public class LocalVideoTestActivity extends Activity {
 
     private VideoPlayerManager mVideoPlayerManager;
+
+    private String mPlayUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
+        this.handleIntent();
         this.init();
+    }
+
+    private void handleIntent() {
+        String action = this.getIntent().getAction();
+
+        if (Intent.ACTION_VIEW.equals(action)) {
+            Uri uri = this.getIntent().getData();
+            LogUtil.d("uri:" + uri);
+
+            if (uri != null) {
+                String scheme = uri.getScheme();
+                LogUtil.d("scheme:" + scheme);
+                if ("content".equals(scheme)) {
+                    Cursor cursor = this.getContentResolver().query(uri,
+                            new String[] { MediaStore.Video.Media.DATA }, null,
+                            null, null);
+                    if (cursor != null) {
+                        int index = cursor
+                                .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+                        if (cursor.moveToFirst()) {
+                            this.mPlayUrl = cursor.getString(index);
+                            return;
+                        }
+                    }
+                } else if ("file".equals(scheme)) {
+                    this.mPlayUrl = uri.getPath();
+                    return;
+                } else {
+                    this.mPlayUrl = uri.toString();
+                    return;
+                }
+            }
+        }
     }
 
     private void init() {
@@ -36,9 +78,11 @@ public class VideoTestActivity extends Activity {
      * 播放测试
      */
     private void testPlay() {
-        this.copyToSdcard();
-        String url = this.DATABASE_PATH + "/" + this.DATABASE_FILENAME;
-        this.mVideoPlayerManager.getPlayController().startPlay(url);
+        if (TextUtils.isEmpty(this.mPlayUrl)) {
+            this.copyToSdcard();
+            this.mPlayUrl = this.DATABASE_PATH + "/" + this.DATABASE_FILENAME;
+        }
+        this.mVideoPlayerManager.getPlayController().startPlay(this.mPlayUrl);
     }
 
     private final String DATABASE_PATH = android.os.Environment
@@ -54,8 +98,8 @@ public class VideoTestActivity extends Activity {
         }
         if (!(new File(databaseFilename)).exists()) {
             try {
-                InputStream is = this.getResources()
-                        .openRawResource(R.raw.test);
+                InputStream is = this.getResources().openRawResource(
+                        R.raw.test);
                 FileOutputStream fos = new FileOutputStream(databaseFilename);
                 byte[] buffer = new byte[8192];
                 int count = 0;
